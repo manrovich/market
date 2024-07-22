9a6d683fa1e9db3eeb972b5e7332f9ab7df503fc
Rompet <r.petrov@smartdata.dev>
2024-07-22 21:58:42 +0800

    initial commit

---
+++ src/test/java/com/hn/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hn.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
